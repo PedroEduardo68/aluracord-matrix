019528ed60caf12aa5cf7a58e1f7e97627ec3986
PedroEduardo68 <pedro.eduardo.camera@gmail.com>
2022-01-29 11:42:27 -0300

    update docker

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "26592ac473dde521"; }
+/******/ 		__webpack_require__.h = function() { return "bb6237b4aa4a3f97"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
